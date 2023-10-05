0c27d8c31945f6811cad205adb8cc3a2436ce75b
Bruno Lopes / <117863700+BrunoLopes24@users.noreply.github.com>
2023-10-05 14:24:23 +0100

    Resolver os erros Jest test

--- API_NodeJS_TS/testes/jest.setup.ts
+++ API_NodeJS_TS/testes/jest.setup.ts
@@ -1,4 +1,14 @@
 import supertest from 'supertest';
 import { app } from '../src/server/app';
+import { Knex } from '../src/server/db/knex';
+import {beforeAll, afterAll} from '@jest/globals';
+
+beforeAll( async () => { // Antes de executar os testes, fará o migration das tabelas.
+    await Knex.migrate.latest();
+});
+
+afterAll (async () => {
+    await Knex.destroy();
+});
 
 export const testServer = supertest(app);
