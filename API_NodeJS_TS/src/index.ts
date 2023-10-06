bfc0e874be9372451a9ee4ccb07856a51957e1c5
Bruno Lopes / <117863700+BrunoLopes24@users.noreply.github.com>
2023-10-06 12:53:49 +0100

    Correr migration de forma automática

--- API_NodeJS_TS/src/index.ts
+++ API_NodeJS_TS/src/index.ts
@@ -1,6 +1,21 @@
 // Importa o servidor e faz mete-se a porta do servidor.
 import { app } from './server/app';
+import { Knex } from './server/db/knex';
 
-app.listen(process.env.PORT||3000,()=> {
-    console.log(`Servidor aberto na porta ${process.env.PORT||3000}`);
-});
+const startServer = () => {
+    app.listen(process.env.PORT||3000,()=> {
+        console.log(`Servidor aberto na porta ${process.env.PORT||3000}`);
+    });
+};
+
+if (process.env.IS_LOCALHOST !== 'true'){
+    Knex.migrate.latest()
+        .then( ()=> {
+            startServer();
+        })
+        .catch(
+            console.log
+        );
+}else {
+    startServer();
+}
