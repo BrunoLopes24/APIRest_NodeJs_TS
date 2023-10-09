023aaed69e87debc5c127cd97fde616e54e89e8f
Bruno Lopes / <117863700+BrunoLopes24@users.noreply.github.com>
2023-10-09 14:39:43 +0100

    Implementando o Cors

--- API_NodeJS_TS/src/server/app.ts
+++ API_NodeJS_TS/src/server/app.ts
@@ -2,11 +2,17 @@
 import express from 'express';
 import 'dotenv/config';
 import router from './routes';
+import cors from 'cors';
 
 //Instancia do servidor
 const app = express();
 
 app.use(express.json());
+app.use(cors({
+    origin: process.env.ENABLE_CORS?.split(';') || []
+}));
+
+
 app.use(router);
 
 //Exporta a instância do servidor
